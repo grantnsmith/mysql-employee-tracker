3c3b0633dcae5426e3de96db17d52e957f4ed9c7
Grant Smith <grantnsmith@gmail.com>
2020-05-17 14:21:33 -0700

    Small updates, pushing before creating next branch.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var consoleTable = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "Graysa21!",
   database: "employees_db"
 });
 
@@ -97,10 +98,11 @@ function startInquirer() {
        if (answer.start === "View total of a departments salaries") {
             viewDepartmentSalaries();
        };
-
-
     })
 };
+
+
+
 
 function viewAllEmployees() {
     console.log("View all employees chosen");
